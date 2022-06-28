89dd7644dda3dc727f6a5ffcd25feecce06e4432
Alden <aldenwangf2e@gmail.com>
2022-06-28 17:24:22 +0800

    [feat]更新productListID,productDetail完成

--- express/routers/productListR.js
+++ express/routers/productListR.js
@@ -5,7 +5,7 @@ const pool = require('../utils/db');
 router.get('/', async (req, res, next) => {
   //   取得product中商品照片、價格、名稱
   let [productList] = await pool.execute(
-    'SELECT product.product_photo, product.price, product.name, product.color_type, product_category.name AS productCategoryName, product_category.level AS productCategoryLevel, product_category.parent_id AS productCategotyParentId FROM product JOIN product_category ON product_category.id = product.product_category_id'
+    'SELECT product.id, product.product_photo, product.price, product.name, product.color_type, product_category.name AS productCategoryName, product_category.level AS productCategoryLevel, product_category.parent_id AS productCategotyParentId FROM product JOIN product_category ON product_category.id = product.product_category_id'
   );
   res.json(productList);
   // console.log(productList);
