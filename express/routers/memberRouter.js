8c277a7a10ffa3b7f7e8dfd190d9a555ca1730ec
robin <robininispan@gmail.com>
2022-06-29 19:30:43 +0800

    [feat] 取得特定 1 位 member 所有的訂單資料 API

--- express/routers/memberRouter.js
+++ express/routers/memberRouter.js
@@ -4,7 +4,6 @@ const pool = require('../utils/db');
 
 // 取得特定 1 位 member 的資料
 router.get('/:memberId', async (req, res, next) => {
-  
   let [member] = await pool.execute(`SELECT * FROM user WHERE id = ?`, [
     req.params.memberId,
   ]);
@@ -22,7 +21,7 @@ router.get('/:memberId', async (req, res, next) => {
     address: member.address,
   };
 
-  res.json({ success: '獲取資料成功！', data: returnMemberInfo });
+  return res.json({ success: '獲取資料成功！', data: returnMemberInfo });
 });
 
 //   修改特定 1 位 member 的資料
@@ -40,8 +39,34 @@ router.patch('/:memberId', async (req, res, next) => {
     ]
   );
 
-
-  res.json({ success: '更新資料成功！' });
+  return res.json({ success: '更新資料成功！' });
 });
 
+// 取得特定 1 位 member 所有的訂單資料
+router.get('/:memberId/orders', async (req, res, next) => {
+  let [orders] = await pool.execute(
+    `
+  SELECT 
+  o.order_id,
+  o.product_id,
+  p.name AS product_name,
+  p.price AS product_price,
+  p.product_photo,
+  o.count,
+  o.create_time AS order_create_time,
+  o.deliver_time AS order_deliver_time,
+  o.finish_time AS order_finish_time,
+  o.is_valid AS order_is_valid
+  FROM orders o
+  JOIN product p
+  ON o.product_id = p.id
+  WHERE user_id = ?
+  GROUP BY o.order_id
+  `,
+    [req.params.memberId]
+  );
+
+
+  return res.json({ success: '獲取所有訂單資料成功！', orders });
+});
 module.exports = router;
