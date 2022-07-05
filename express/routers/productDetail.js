26a8dbd4202fce982e72a74e9b1d9caa18c24b2e
Alden <aldenwangf2e@gmail.com>
2022-07-05 18:16:37 +0800

    [feat]productDetail商品評論

--- express/routers/productDetail.js
+++ express/routers/productDetail.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db');
 
-router.get('/', async (req, res, next) => {
+router.get('/products', async (req, res, next) => {
   //   取得product中商品照片、價格、名稱
   let [productdetail] = await pool.execute(
     `SELECT comment.* ,product.*, orders.user_id AS ordersUser_id, user.name AS userName, user.phone AS userPhoto FROM comment JOIN product ON  comment.product_id = product.id JOIN orders ON comment.orders_id = orders.id JOIN user ON orders.user_id =user.id 
     `
   );
-  console.log(productdetail);
+  res.json(productdetail);
+});
+router.get('/products/:productsId', async (req, res, next) => {
+  let [productdetail] = await pool.execute(
+    'SELECT comment.* ,product.*, orders.user_id AS ordersUser_id, user.name AS userName, user.phone AS userPhoto FROM comment JOIN product ON  comment.product_id = product.id JOIN orders ON comment.orders_id = orders.id JOIN user ON orders.user_id =user.id WHERE product.id = ?',
+    [req.params.productsId]
+  );
   res.json(productdetail);
 });
 
