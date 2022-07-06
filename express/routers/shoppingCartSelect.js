d52d0e20d65ef3f978f9dd8089d8c88daedf5eae
Alden <aldenwangf2e@gmail.com>
2022-07-06 10:58:45 +0800

    [Update]shoppingCart gift_card WHERE is_used

--- express/routers/shoppingCartSelect.js
+++ express/routers/shoppingCartSelect.js
@@ -2,12 +2,12 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db');
 
-router.get('/menber/:menberId', async (req, res, next) => {
+router.get('/:menberId', async (req, res, next) => {
   let [user] = await pool.execute('SELECT * FROM user WHERE user.id = ?', [
     req.params.menberId,
   ]);
   let [giftCardList] = await pool.execute(
-    'SELECT * FROM gift_card WHERE gift_card.receiver_email = ?',
+    'SELECT * FROM gift_card WHERE gift_card.is_used = 0 AND gift_card.receiver_email = ?',
     [user[0].email]
   );
 
