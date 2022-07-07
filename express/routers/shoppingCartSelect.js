bcb289a2bf12b096437df00ac162f00fcc31e960
Alden <aldenwangf2e@gmail.com>
2022-07-07 14:59:11 +0800

    [feat]shoppingCart 新增禮物卡訂單

--- express/routers/shoppingCartSelect.js
+++ express/routers/shoppingCartSelect.js
@@ -2,8 +2,43 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db');
 
+// 新增訂單
+router.post('/uploadOrder', async (req, res, next) => {
+  // orderList
+  const { v4: uuidv4 } = require('uuid');
+  const orderId = uuidv4();
+  // console.log(req.body.giftCardlist[1].gift_card_id);
+  for (let i = 0; i < req.body.productlist.length; i++) {
+    let [reponse] = await pool.execute(
+      `INSERT INTO orders( order_id, user_id, product_id, gift_card_id, count) VALUES ( ? , ? , ? , ? , ?)`,
+      [
+        orderId,
+        req.body.memberId,
+        req.body.productlist[i].product_id,
+        req.body.gift_card_id,
+        req.body.productlist[i].count,
+      ]
+    );
+  }
+  for (let i = 0; i < req.body.productlist.length; i++) {
+    let [reponse] = await pool.execute(
+      `INSERT INTO orders( order_id, user_id, product_giftcard_id, gift_card_id) VALUES ( ? , ? , ? , ? )`,
+      [
+        orderId,
+        req.body.memberId,
+        req.body.giftCardlist[1].gift_card_id,
+        req.body.gift_card_id,
+      ]
+    );
+    console.log(reponse);
+  }
+
+  return res.json({ success: '已新增至我的訂單！' });
+});
+
+// 找特定會員購物車資料
 router.get('/:menberId', async (req, res, next) => {
-  console.log('in');
+  // console.log('in');
   let [user] = await pool.execute('SELECT * FROM user WHERE user.id = ?', [
     req.params.menberId,
   ]);
@@ -13,22 +48,6 @@ router.get('/:menberId', async (req, res, next) => {
   );
 
   res.json({ user, giftCardList });
-});
-
-// 新增訂單
-router.post('/:memberId/uploadOrder', async (req, res, next) => {
-  let [reponse] = await pool.execute(
-    `INSERT INTO orders( order_id, user_id, product_id, gift_card_id, count) VALUES ( ? , ? , ? , ? , ?)`,
-    [
-      req.params.order_id,
-      req.params.memberId,
-      req.params.product_id,
-      req.params.gift_card_id,
-      req.params.count,
-    ]
-  );
-
-  return res.json({ success: '已新增至我的訂單！' });
 });
 
 module.exports = router;
