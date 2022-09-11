589b829e086e936651cb5261cdb021932d6de9a3
Robin <robininispan@gmail.com>
2022-09-11 13:50:16 +0800

    [update] 撈產品 API productListRouter 改為用 get 方式接收配合前端

--- routers/productListRouter.js
+++ routers/productListRouter.js
@@ -2,15 +2,15 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../utils/db');
 
-router.post('/', async (req, res, next) => {
-  // 取得 product
-
-  const { product_category_id, product_category_level } = req.body;
-  // 定義篩選條件，若前端有傳來 product_category_level 就用 level 去篩選
-  // 若沒有則用 id 篩選
-  const filterStatement = product_category_level
-    ? `WHERE pc.level = ${product_category_level}`
-    : `WHERE p.product_category_id = ${product_category_id}`;
+router.get('/', async (req, res, next) => {
+  const { product_category_id, product_category_level } = req.query;
+  // 定義篩選條件，若前端有傳來 product_category_level 不為 0 就用 level 去篩選
+  // 若 product_category_level 為 0 則用 id 篩選
+  const filterStatement =
+    // 注意！用前端用 get 傳 query string 過來都是字串！故下面判斷是 0 要加上引號
+    product_category_level !== '0'
+      ? `WHERE pc.level = ${product_category_level}`
+      : `WHERE p.product_category_id = ${product_category_id}`;
 
   let [products] = await pool.execute(
     `SELECT
